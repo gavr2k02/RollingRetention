f15442fb015f5c994a903bebe46930760f17f41d
gavr2k02 <gavrilin2k02@gmail.com>
2021-03-28 20:02:12 +0300

    v 1.0

--- RollingRetention/Services/RollingRetentionService.cs
+++ RollingRetention/Services/RollingRetentionService.cs
@@ -24,6 +24,114 @@ namespace RollingRetention.Services
         {
             try
             {
+                return CalculateRollingRetention7Day(users);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+                return new List<DataRollingRetention7Day>();
+            }
+        }
+
+        public IEnumerable<DataRollingRetention7Day> GetDataRollingRetention7DayFromDB()
+        {
+            try
+            {
+                return CalculateRollingRetention7Day(userRepository.GetUsers());
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+                return new List<DataRollingRetention7Day>();
+            }
+        }
+
+        public IEnumerable<DataRollingRetentionXDay> GetDataRollingRetentionXDayFromClient(IEnumerable<User> users)
+        {
+            try
+            {
+                return CalculateDataRollingRetentionXDay(users);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+                return new List<DataRollingRetentionXDay>();
+            }
+        }
+
+        public IEnumerable<DataRollingRetentionXDay> GetDataRollingRetentionXDayFromDB()
+        {
+            try
+            {
+                return CalculateDataRollingRetentionXDay(userRepository.GetUsers());
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+                return new List<DataRollingRetentionXDay>();
+            }
+        }
+
+        public IEnumerable<DataRollingRetentionXDay> CalculateDataRollingRetentionXDay(IEnumerable<User> users)
+        {
+            try
+            {
+                var dataRRXDs = new List<DataRollingRetentionXDay>();
+
+                for (int i = 0; i < maxDay; i++)
+                {
+                    double countBack = 0;
+                    double countInstall = 0;
+
+                    foreach (var user in users)
+                    {
+                        if (i <= (user.DateLastActivity - user.DateRegistration).Days)
+                            countBack++;
+
+                        if (user.DateRegistration >= user.DateLastActivity.AddDays(-i)) ;
+                        countInstall++;
+                    }
+
+                    if (countInstall == 0 || countBack == 0)
+                        return dataRRXDs;
+
+                    dataRRXDs.Add(new DataRollingRetentionXDay()
+                    {
+                        Day = i,
+                        Percent = Math.Round(countBack / countInstall * 100, 2)
+                    });
+                }
+
+                return dataRRXDs;
+
+                // var dataRRXDs = new List<DataRollingRetentionXDay>();
+
+                // for (int i = 0; i < maxDay; i++)
+                // {
+                //     double countBack = users.Where(user => i <= (user.DateLastActivity - user.DateRegistration).Days).Count();
+                //     double countInstall = users.Where(user => (user.DateLastActivity.AddDays(-i) <= user.DateRegistration)).Count();
+
+                //     if (countBack == 0 || countInstall == 0)
+                //         return dataRRXDs;
+                //     else
+                //         dataRRXDs.Add(new DataRollingRetentionXDay()
+                //         {
+                //             Day = i,
+                //             Percent = Math.Round(countBack / countInstall * 100, 2)
+                //         });
+                // }
+                // return dataRRXDs;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public IEnumerable<DataRollingRetention7Day> CalculateRollingRetention7Day(IEnumerable<User> users)
+        {
+            try
+            {
                 var dataRR7Ds = new List<DataRollingRetention7Day>();
 
                 foreach (var user in users)
@@ -37,115 +145,7 @@ namespace RollingRetention.Services
             }
             catch (Exception ex)
             {
-                logger.LogError(ex.Message);
-                return new List<DataRollingRetention7Day>();
-            }
-        }
-
-        public IEnumerable<DataRollingRetention7Day> GetDataRollingRetention7DayFromDB()
-        {
-            try
-            {
-                var dataRR7Ds = new List<DataRollingRetention7Day>();
-
-                foreach (var user in userRepository.GetUsers())
-                    dataRR7Ds.Add(new DataRollingRetention7Day()
-                    {
-                        Id = user.Id,
-                        UserLifespan = (user.DateLastActivity - user.DateRegistration).Days
-                    });
-
-                return dataRR7Ds;
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex.Message);
-                return new List<DataRollingRetention7Day>();
-
-            }
-        }
-
-        public IEnumerable<DataRollingRetentionXDay> GetDataRollingRetentionXDayFromClient(IEnumerable<User> users)
-        {
-            try
-            {
-                var dataRRXDs = new List<DataRollingRetentionXDay>();
-
-                for (int i = 0; i < maxDay; i++)
-                {
-                    double countBack = users.Where(user => i <= (user.DateLastActivity - user.DateRegistration).Days).Count();
-                    double countInstall = users.Where(user => user.DateRegistration >= user.DateLastActivity.AddDays(-i)).Count();
-
-                    if (countBack == 0 || countInstall == 0)
-                        return dataRRXDs;
-                    else
-                        dataRRXDs.Add(new DataRollingRetentionXDay()
-                        {
-                            Day = i,
-                            Percent = countBack / countInstall * 100
-                        });
-                }
-                return dataRRXDs;
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex.Message);
-                return new List<DataRollingRetentionXDay>();
-            }
-        }
-
-        public IEnumerable<DataRollingRetentionXDay> GetDataRollingRetentionXDayFromDB()
-        {
-            try
-            {
-                // var dataRRXDs = new List<DataRollingRetentionXDay>();
-                // var users = userRepository.GetUsers();
-
-                // for (int i = 0; i < maxDay; i++)
-                // {
-                //     double countBack = 0;
-                //     double countInstall = 0;
-
-                //     foreach (var user in users)
-                //     {
-                //         if (i <= (user.DateLastActivity - user.DateRegistration).Days)
-                //             countBack++;
-
-                //         if (user.DateRegistration >= user.DateLastActivity.AddDays(-i)) ;
-                //         countInstall++;
-                //     }
-
-                //     if (countInstall == 0 || countBack == 0)
-                //         break;
-                //     double percent = countBack / countInstall;
-                //     dataRRXDs.Add(new DataRollingRetentionXDay() { Day = i, Percent = percent * 100 });
-                // }
-
-                // return dataRRXDs;
-
-                var dataRRXDs = new List<DataRollingRetentionXDay>();
-                var users = userRepository.GetUsers();
-
-                for (int i = 0; i < maxDay; i++)
-                {
-                    double countBack = users.Where(user => i <= (user.DateLastActivity - user.DateRegistration).Days).Count();
-                    double countInstall = users.Where(user => user.DateRegistration >= user.DateLastActivity.AddDays(-i)).Count();
-
-                    if (countBack == 0 || countInstall == 0)
-                        return dataRRXDs;
-                    else
-                        dataRRXDs.Add(new DataRollingRetentionXDay()
-                        {
-                            Day = i,
-                            Percent = countBack / countInstall * 100
-                        });
-                }
-                return dataRRXDs;
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex.Message);
-                return new List<DataRollingRetentionXDay>();
+                throw;
             }
         }
     }
